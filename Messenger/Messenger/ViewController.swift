330d46fe772691804e9bbc8cdcb355db27da84a3
Afrikanov Aleksandr <43299725+afrikanov@users.noreply.github.com>
2020-03-26 02:59:39 +0700

    Profile page v.1

--- Messenger/Messenger/ViewController.swift
+++ Messenger/Messenger/ViewController.swift
@@ -8,38 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var imageSelectButton: UIButton!
+    
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0) -> UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8) / 256.0
+        let blue = CGFloat(rgbValue & 0xFF) / 256.0
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+    
+    let radius: CGFloat = 39
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#function)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print(#function)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print(#function)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        print(#function)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        print(#function)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print(#function)
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print(#function)
+        editButton.layer.cornerRadius = 10
+        editButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        editButton.layer.borderWidth = 3
+        profileImage.layer.cornerRadius = radius
+        imageSelectButton.backgroundColor = UIColorFromHex(rgbValue: 0x3F78F0)
+        imageSelectButton.layer.cornerRadius = radius
     }
 }
